a4811112d29909b83bf578e92e272b753704b649
Sankalp Dayal <sadayal@invensense.com>
2018-02-01 19:57:45 -0800

    Increased process noise for px, py

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -46,10 +46,10 @@ FusionEKF::FusionEKF() {
 			  0, 0, 0, 1;
 			  
   ekf_.P_ = MatrixXd(4, 4);		  
-  ekf_.P_ << 1000, 0, 0, 0,
-			  0, 1000, 0, 0,
-			  0, 0, 1000, 0,
-			  0, 0, 0, 1000;
+  ekf_.P_ << 100, 0, 0, 0,
+			  0, 100, 0, 0,
+			  0, 0, 1, 0,
+			  0, 0, 0, 1;
   ekf_.Q_ = MatrixXd(4, 4);
 
 }
@@ -83,13 +83,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
 	  float px = measurement_pack.raw_measurements_[0]*cos(measurement_pack.raw_measurements_[1]);
 	  float py = measurement_pack.raw_measurements_[0]*sin(measurement_pack.raw_measurements_[1]);
-	  ekf_.x_ << px, py, 1.0, 1.0;
+	  ekf_.x_ << px, py, 5.0, 0.0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
-	  ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 1.0, 1.0;
+	  ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 5.0, 0.0;
     }
 	previous_timestamp_ = measurement_pack.timestamp_;
 
