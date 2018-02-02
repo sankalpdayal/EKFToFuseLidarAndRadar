dda209ac5bb3a540850262d97adedf96ed48d8b6
Sankalp Dayal <sadayal@invensense.com>
2018-02-02 11:37:42 -0800

    Fixing bug in KF update

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -42,6 +42,12 @@ void KalmanFilter::Update(const VectorXd &z) {
 	MatrixXd Si = S.inverse();
 	MatrixXd PHt = P_ * Ht;
 	MatrixXd K = PHt * Si;
+	
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
