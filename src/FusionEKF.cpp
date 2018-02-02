a9777736e4d3162454fbc440d17bf2dd2b528610
Sankalp Dayal <sadayal@invensense.com>
2018-02-02 10:57:45 -0800

    Correcting process noise values

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -116,8 +116,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.F_(0, 2) = dt;
 	ekf_.F_(1, 3) = dt;
 	
-	float noise_ax = 5.0;
-	float noise_ay = 5.0;
+	float noise_ax = 9.0;
+	float noise_ay = 9.0;
 	float dt_2 = dt * dt;
 	float dt_3 = dt_2 * dt;
 	float dt_4 = dt_3 * dt;
